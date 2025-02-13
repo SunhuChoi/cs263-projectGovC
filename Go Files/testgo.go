1ba5cf5adb8186416ebd9b23d377d2dbf77786a8
rainakakani21 <rkakani@ucsb.edu>
2025-02-13 15:00:37 -0800

    quicksort in go

--- Go Files/testgo.go
+++ Go Files/testgo.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"customfunctions"
-	"math/rand"
-	"time"
+    "customfunctions"
+    "math/rand"
+    "time"
+    "fmt"
 ) 
 
 const (
-    ROWVAL = 1000
-    COLVAL = 100
+    ROWVAL = 10
+    COLVAL = 10
 )
 
 var log = customfunctions.InitLogger()
@@ -31,20 +32,22 @@ func binarySearch(arr []int, l int, r int, x int) int {
     return -1
 }
 
-func createArray(size int) []int {
-    arr := make([]int, size)
-    for i := 0; i < size; i++ {
-        arr[i] = i
-    }
-    return arr
-}
+// func createArray(size int) []int { 
+//     arr := make([]int, size)
+//     for i := 0; i < size; i++ {
+//         arr[i] = i
+//     }
+//     return arr
+// }
 
 func create2DArray(row int, col int) [][] int {
+    rand.Seed(time.Now().UnixNano()) // seed the rand
+
     arr := make([][] int, row)
     for i:= 0; i < row; i++ {
         arr[i] = make([]int, col)
         for j := 0; j <col; j++ {
-            arr[i][j] = rand.Intn(row*col)
+            arr[i][j] = rand.Intn(10)
         }
     }
     return arr
@@ -59,10 +62,25 @@ func testBasicArraySearch() {
 
     start2 := time.Now()
     for i:= 0; i < ROWVAL; i++ {
-        binarySearch(arr[i], 0, COLVAL-1, rand.Intn(ROWVAL*COLVAL))
+        customfunctions.QuickSort(arr[i], 0, COLVAL-1)
+        res := binarySearch(arr[i], 0, COLVAL-1, rand.Intn(10))
+        if res != -1 {
+            fmt.Print("found the target number at index ", res, "\n")
+        } else {
+            fmt.Print("target number not found for this row \n")
+        }
     }
     end2 := time.Now()
-    log.Println("Binary Search Runtime: ", end2.Sub(start2))
+    log.Println("Binary Search and Quicksort Runtime: ", end2.Sub(start2))
+
+    for i:=0; i < ROWVAL; i++ {
+        for j:=0; j < COLVAL; j++ {
+            fmt.Print(arr[i][j])
+            fmt.Print(" ")
+        }
+        fmt.Println("")
+    }
+
 }
 
 func main() {
