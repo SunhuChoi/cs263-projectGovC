e9e195973bd02ed6619800e0b01392b1ef17cf43
rainakakani21 <rkakani@ucsb.edu>
2025-02-21 16:02:47 -0800

    insertion, bubble, and iterative fib

--- Go Files/custom-functions/useful_funcs.go
+++ Go Files/custom-functions/useful_funcs.go
@@ -121,4 +121,49 @@ func FibRecursive(n int, dp map[int]int) int{
 	return FibRecursive(n-2, dp) + FibRecursive(n-1, dp)
 }
 
-// func FibIterative()
+
+func InsertionSort(arr []int) {
+    leng := len(arr)
+
+    for i := 1; i < leng; i++ {
+        key := arr[i]
+        j := i - 1
+
+        for j >= 0 && arr[j] > key {
+            arr[j+1] = arr[j]
+            j--
+        }
+
+        arr[j+1] = key
+    }
+}
+
+
+func BubbleSort(arr []int) {
+    leng := len(arr)
+
+    for i := 0; i < leng-1; i++ {
+
+        for j := 0; j < leng-i-1; j++ {
+            if arr[j] > arr[j+1] {
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            }
+        }
+
+    }
+}
+
+
+func FibIterative(n int) int {
+    if n <= 1 {
+        return n
+    }
+
+    a, b := 0, 1
+	
+    for i := 2; i <= n; i++ {
+        a, b = b, a+b
+    }
+
+    return b
+}
