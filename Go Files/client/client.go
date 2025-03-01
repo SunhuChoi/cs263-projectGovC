7ef226f106b8561ef96c5577869e651ac06bac99
Sunhu Choi <sunhu@ucsb.edu>
2025-02-28 18:04:21 -0800

    Working protoype of hangman done

--- Go Files/client/client.go
+++ Go Files/client/client.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	cf "gofiles/custom-functions"
 	"net"
+	"strings"
 )
 
 var log = cf.InitLogger()
@@ -15,5 +17,50 @@ func main() {
 	}
 
 	log.Println("Connected!")
-	con.Close()
+
+	defer con.Close()
+	totGuesses := 0
+
+	for totGuesses < 5 {
+		recv := make([]byte, 1024)
+		n, err := con.Read(recv)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	
+		result := strings.TrimSpace(string(recv[:n]))
+		log.Println(result)
+		if strings.Contains(result, "You won!") {
+			break
+		}
+
+		var guess string
+		fmt.Scanln(&guess)
+		_, err = con.Write([]byte(guess))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		recv = make([]byte, 1024)
+		_, err = con.Read(recv)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(strings.TrimSpace(string(recv[:n])))
+		totGuesses++
+	}
+
+	recv := make([]byte, 1024)
+	n, err := con.Read(recv)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	result := strings.TrimSpace(string(recv[:n]))
+	log.Println(result)
+
 }
--- Go Files/server/server.go
+++ Go Files/server/server.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"fmt"
 	cf "gofiles/custom-functions"
 	"math/rand"
 	"net"
+	"slices"
 	"strings"
 	"sync"
 ) 
 
 var log = cf.InitLogger()
 var m sync.Mutex
+var numClients int = 0
 
 func start() {
 	
@@ -20,16 +23,16 @@ func start() {
 	}
 
 	defer l.Close()
-	numClients := 0
 	for {
-		if numClients == 2 {
-			log.Println("Max clients!")
-			continue
-		}
 		con, err := l.Accept()
 		if err != nil {
 			log.Println(err)
 			return
+		}
+
+		if numClients == 2 {
+			log.Println("Max clients!")
+			continue
 		}
 
 		go handler(con)
@@ -44,43 +47,81 @@ func start() {
 func handler(conn net.Conn) {
 	defer conn.Close()
 	guessedIndexes := make([]int, 0)
-	words := []string{"chicken", "awebo", "mourning dove", "loon"}
+	words := []string{"loon", "mourningdove", "shimaenaga", "awebo", "chicken"}
 	index := rand.Intn(len(words))
 
 	word := words[index]
 	numGuesses := 0
 
-	for len(guessedIndexes) != len(words) || numGuesses < 5 {
-		log.Println("Your word is!: ")
+	for len(guessedIndexes) != len(word) && numGuesses < 5 {
+		bufferMessage := "Your word is!: \n"
 		for i:= 0; i < len(word); i++ {
-			log.Print(" _ ")
+			if slices.Contains(guessedIndexes, i) {
+				bufferMessage += fmt.Sprintf(" %c ", word[i])
+			} else {
+				bufferMessage += " _ "
+			}
 		}
-		log.Println()
-		log.Println("Guess a character in the word!")
+		bufferMessage += "\nGuess a character in the word!"
 
-		recv := make([]byte, 1024)
-		_, err := conn.Read(recv)
+		// First write that is sent 
+		_, err := conn.Write([]byte(bufferMessage))
+		
 		if err != nil {
 			log.Println(err)
 			return
 		}
 
-		guess := string(recv)
+		recv := make([]byte, 1024)
+		n, err := conn.Read(recv)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		guess := strings.TrimSpace(string(recv[:n]))
+		fmt.Printf("What %s", guess)
 		check := 0
 
 		prevGuessCount := len(guessedIndexes)
+		tempWord := []rune(word)
 		for check != -1 {
-			check = strings.Index(guess, word[check:])
+			fmt.Println("Entered!")
+			check = strings.Index(string(tempWord), guess)
+			fmt.Println(check)
 			if check != -1 {
 				guessedIndexes = append(guessedIndexes, check)
+				tempWord[check] = '_'
 			}
 		}
 
+		result := ""
 		if len(guessedIndexes) > prevGuessCount {
-			log.Println("You guessed a correct letter!")
+			result = "You guessed a correct letter!"
 		} else {
-			log.Println("oof")
+			result = "Oof"
 		}
+		// Second write that is sent 
+		_, err = conn.Write([]byte(result))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		numGuesses++
+	}
+	
+	finished := ""
+	if numGuesses == 5 && len(guessedIndexes) != len(word) {
+		finished = "Maybe next time!"
+	} else {
+		finished = fmt.Sprintf("You won! The word was %s", word)
+	}
+
+	// Third write that is sent 
+	_, err := conn.Write([]byte(finished))
+	if err != nil {
+		log.Println(err)
+		return
 	}
 }
 
