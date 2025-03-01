1dfec3d2dd2912a83cafbb6cd07bb2f9b03ab8ff
Sunhu Choi <sunhu@ucsb.edu>
2025-02-28 16:47:51 -0800

    Adjusting server

--- Go Files/server/server.go
+++ Go Files/server/server.go
@@ -4,6 +4,7 @@ import (
 	cf "gofiles/custom-functions"
 	"math/rand"
 	"net"
+	"strings"
 	"sync"
 ) 
 
@@ -42,16 +43,45 @@ func start() {
 
 func handler(conn net.Conn) {
 	defer conn.Close()
+	guessedIndexes := make([]int, 0)
 	words := []string{"chicken", "awebo", "mourning dove", "loon"}
 	index := rand.Intn(len(words))
 
-	log.Println("Your word is!: ")
-	for i:= 0; i < index; i++ {
-		log.Print(" _ ")
-	}
-	log.Println()
-	log.Println("Guess the word!")
+	word := words[index]
+	numGuesses := 0
 
+	for len(guessedIndexes) != len(words) || numGuesses < 5 {
+		log.Println("Your word is!: ")
+		for i:= 0; i < len(word); i++ {
+			log.Print(" _ ")
+		}
+		log.Println()
+		log.Println("Guess a character in the word!")
+
+		recv := make([]byte, 1024)
+		_, err := conn.Read(recv)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		guess := string(recv)
+		check := 0
+
+		prevGuessCount := len(guessedIndexes)
+		for check != -1 {
+			check = strings.Index(guess, word[check:])
+			if check != -1 {
+				guessedIndexes = append(guessedIndexes, check)
+			}
+		}
+
+		if len(guessedIndexes) > prevGuessCount {
+			log.Println("You guessed a correct letter!")
+		} else {
+			log.Println("oof")
+		}
+	}
 }
 
 
